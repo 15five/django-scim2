380f7ad0bf31f20e98b134a8a50e3399efcf5554
Paul Logston <code@logston.me>
2016-12-05 18:46:36 -0500

    Add tests for adapters

--- django_scim/tests/test_models.py
+++ django_scim/tests/test_models.py
@@ -6,17 +6,45 @@ from django.test import TestCase
 from django_scim.utils import get_user_adapter
 from django_scim.utils import get_group_adapter
 
+from django_scim.models import SCIMServiceProviderConfig
+
 
 class SCIMServiceProviderConfigTestCase(TestCase):
+    maxDiff = None
+
     def test_authentication_schemes(self):
-        self.fail()
+        config = SCIMServiceProviderConfig()
+        self.assertEqual(config.authentication_schemes, [])
 
     def test_meta(self):
-        self.fail()
+        config = SCIMServiceProviderConfig()
+        expected = {
+            'resourceType': 'ServiceProviderConfig',
+            'location': u'https://localhost/scim/v2/ServiceProviderConfig',
+        }
+        self.assertEqual(config.meta, expected)
 
     def test_location(self):
-        self.fail()
+        config = SCIMServiceProviderConfig()
+        location = 'https://localhost/scim/v2/ServiceProviderConfig'
+        self.assertEqual(config.location, location)
 
     def test_to_dict(self):
-        self.fail()
+        config = SCIMServiceProviderConfig()
+        expected ={
+            'authenticationSchemes': [],
+            'bulk': {'supported': False},
+            'changePassword': {'supported': True},
+            'documentationUri': None,
+            'etag': {'supported': False},
+            'filter': {'supported': True},
+            'meta': {
+                'location': u'https://localhost/scim/v2/ServiceProviderConfig',
+                'resourceType': 'ServiceProviderConfig'
+            },
+            'patch': {'supported': True},
+            'schemas': ['urn:ietf:params:scim:schemas:core:2.0:ServiceProviderConfig'],
+            'sort': {'supported': False}
+        }
+        self.assertEqual(config.to_dict(), expected)
 
