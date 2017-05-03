bb9e1aa3695fb52278fc910ce96aa6fcf1630bc0
Paul Logston <code@logston.me>
2017-05-03 17:12:38 -0400

    Allow trailing slash in urls

--- django_scim/urls.py
+++ django_scim/urls.py
@@ -12,43 +12,43 @@ urlpatterns = [
         views.SCIMView.as_view(implemented=False),
         name='root'),
 
-    url(r'^.search$',
+    url(r'^.search/?$',
         views.SearchView.as_view(implemented=False),
         name='search'),
 
-    url(r'^Users/.search$',
+    url(r'^Users/.search/?$',
         views.SearchView.as_view(scim_adapter=get_user_adapter(), parser=SCIMUserFilterTransformer),
         name='users-search'),
 
-    url(r'^Users(?:/(?P<uuid>[^/]+))?$',
+    url(r'^Users(?:/(?P<uuid>[^/]+))?/?$',
         views.UsersView.as_view(),
         name='users'),
 
-    url(r'^Groups/.search$',
+    url(r'^Groups/.search/?$',
         views.SearchView.as_view(implemented=False),
         name='groups-search'),
 
-    url(r'^Groups(?:/(?P<uuid>[^/]+))?$',
+    url(r'^Groups(?:/(?P<uuid>[^/]+))?/?$',
         views.GroupsView.as_view(),
         name='groups'),
 
-    url(r'^Me$',
+    url(r'^Me/?$',
         views.SCIMView.as_view(implemented=False),
         name='me'),
 
-    url(r'^ServiceProviderConfig$',
+    url(r'^ServiceProviderConfig/?$',
         views.ServiceProviderConfigView.as_view(),
         name='service-provider-config'),
 
-    url(r'^ResourceTypes(?:/(?P<uuid>[^/]+))?$',
+    url(r'^ResourceTypes(?:/(?P<uuid>[^/]+))?/?$',
         views.ResourceTypesView.as_view(),
         name='resource-types'),
 
-    url(r'^Schemas(?:/(?P<uuid>[^/]+))?$',
+    url(r'^Schemas(?:/(?P<uuid>[^/]+))?/?$',
         views.SchemasView.as_view(),
         name='schemas'),
 
-    url(r'^Bulk$',
+    url(r'^Bulk/?$',
         views.SCIMView.as_view(implemented=False),
         name='bulk'),
 ]
