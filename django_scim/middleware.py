b320c19c12f8fbaf206ef96eb6a6e6a54fbe4224
Paul Logston <code@logston.me>
2017-08-24 16:03:23 -0400

    Update middleware

--- django_scim/middleware.py
+++ django_scim/middleware.py
@@ -1,25 +1,25 @@
 from django.core.urlresolvers import reverse
 from django.http.response import HttpResponse
 
-# bastb Django 1.10 has updated Middleware. This code imports the Mixin required to get old-style
-# middleware working again
-# More?
-#  https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
-try:
-    from django.utils.deprecation import MiddlewareMixin
-    middleware_parent_class = MiddlewareMixin
-except ImportError:
-    middleware_parent_class = object
-
 from .settings import scim_settings
 
 
-class SCIMAuthCheckMiddleware(middleware_parent_class):
+class SCIMAuthCheckMiddleware(object):
     """
     Check to see if a prior middleware has logged the user in.
 
     This middleware should be place after auth middleware used to login a user.
     """
+
+    def __init__(self, get_response=None):
+        # One-time configuration and initialization.
+        self.get_response = get_response
+        super(SCIMAuthCheckMiddleware, self).__init__()
+
+    def __call__(self, request):
+        self.process_request(request)
+        return self.get_response(request)
+
     def process_request(self, request):
         # If we've just passed through the auth middleware and there is no user
         # associated with the request we can assume permission
