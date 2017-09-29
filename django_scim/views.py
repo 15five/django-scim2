71fa3924d275a58884b2f288bb75fed173e40694
Robert Bruce Park <robert.park@telmediq.com>
2017-09-29 11:11:02 -0700

    Move assertion above where KeyError would be raised.

--- django_scim/views.py
+++ django_scim/views.py
@@ -48,17 +48,17 @@ class SCIMView(View):
         # Perform the lookup filtering.
         lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
 
-        uuid = self.kwargs[lookup_url_kwarg]
-
-        extra_filter_kwargs = self.get_extra_filter_kwargs(self.request, uuid)
-        extra_filter_kwargs[self.lookup_field] = uuid
-
         assert lookup_url_kwarg in self.kwargs, (
             'Expected view %s to be called with a URL keyword argument '
             'named "%s". Fix your URL conf, or set the `.lookup_field` '
             'attribute on the view correctly.' %
             (self.__class__.__name__, lookup_url_kwarg)
         )
+
+        uuid = self.kwargs[lookup_url_kwarg]
+
+        extra_filter_kwargs = self.get_extra_filter_kwargs(self.request, uuid)
+        extra_filter_kwargs[self.lookup_field] = uuid
 
         try:
             return self.model_cls.objects.get(**extra_filter_kwargs)
