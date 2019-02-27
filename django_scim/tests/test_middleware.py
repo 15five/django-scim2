33220acd78f5dcd51a0aa2d30c87b3845f6b7ef0
Paul Logston <paul.logston@gmail.com>
2019-02-27 11:31:30 -0500

    Only log for SCIM requests

--- django_scim/tests/test_middleware.py
+++ django_scim/tests/test_middleware.py
@@ -1,3 +1,8 @@
+import six
+if six.PY3:
+    from unittest import mock
+else:
+    import mock
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 
@@ -9,11 +14,23 @@ class SCIMMiddlewareTestCase(TestCase):
         """
         Regression test for https://github.com/15five/django-scim2/issues/13
         """
-        check = SCIMAuthCheckMiddleware()
+        middleware = SCIMAuthCheckMiddleware()
 
-        request = RequestFactory().get(check.reverse_url)
+        request = RequestFactory().get(middleware.reverse_url)
         request.user = AnonymousUser()
 
-        response = check.process_request(request)
+        response = middleware.process_request(request)
         self.assertEqual(response.status_code, 401)
+
+    @mock.patch('django_scim.middleware.SCIMAuthCheckMiddleware.log_call')
+    def test_log_called_only_for_scim_calls(self, log_call_func):
+        middleware = SCIMAuthCheckMiddleware()
+
+        request = RequestFactory().get('/')
+        middleware.process_response(request, None)
+        log_call_func.assert_not_called()
+
+        request = RequestFactory().get(middleware.reverse_url)
+        middleware.process_response(request, None)
+        log_call_func.assert_called()
 
