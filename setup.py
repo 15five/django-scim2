b284032197e98682bafd7a0608c8fdd91cfc37f5
Paul Logston <paul.logston@gmail.com>
2019-07-09 19:01:17 -0400

    Version bump => 0.12.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def run_tests():
 
 setup(
     name='django-scim2',
-    version='0.12.3',
+    version='0.12.4',
     description='A partial implementation of the SCIM 2.0 provider specification for use with Django.',
     url='https://github.com/15five/django-scim2',
     download_url='https://github.com/15five/django-scim2/archive/master.zip',
