468e1fce8463ed15bc17a88189acb82fc183a4ed
Colin Powell <colin@unbl.ink>
2022-06-02 14:16:54 -0400

    Version Bump => 0.17.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def run_tests():
 
 setup(
     name='django-scim2',
-    version='0.17.2',
+    version='0.17.3',
     description='A partial implementation of the SCIM 2.0 provider specification for use with Django.',
     url='https://github.com/15five/django-scim2',
     download_url='https://github.com/15five/django-scim2/archive/master.zip',
